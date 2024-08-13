2b1af63088fb1a2389cd29b9fbf9011473454215
Juan Ignacio Sierra <juanignaciosierra96@gmail.com>
2024-08-13 16:38:55 +0200

    Added recipient address

--- scripts/mint.js
+++ scripts/mint.js
@@ -4,7 +4,7 @@ module.exports = async callback => {
   const instance = await AFNinjaToken.deployed();
   console.log('Minting on contract:', instance.address);
   //const tx = await instance.mint(ADDRESS, TOKEN_ID , METADATA_URI);
-  const tx = await instance.mint("0x110e9E680E548499Aef8C143b5B841b029ee395D", TOKEN_ID , "ipfs://QmXGUrktc4tWwV31Zqwaj9XvP3QpJyUw3Fs9A2gGjJ87at");
+  const tx = await instance.mint(RECEPIENT_ADDRESS, TOKEN_ID , "ipfs://QmXGUrktc4tWwV31Zqwaj9XvP3QpJyUw3Fs9A2gGjJ87at");
 
   callback(tx.tx);
 }
